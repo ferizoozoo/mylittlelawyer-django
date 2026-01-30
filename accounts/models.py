c6dafd82cc82c9e1d4bac39103cfe6b0b2bd7844
Abtinz <Abtinzandi@gmail.com>
2026-01-30 15:44:46 -0500

    debugging, better email field choice

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,12 @@
 import uuid
+
 from django.db import models
-from django.contrib.postgres.fields import CIEmailField
+from django.db.models.functions import Lower
 
 class User(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     full_name = models.TextField()
-    email = CIEmailField(unique=True)
+    email = models.EmailField()
     password_hash = models.TextField(blank=True, null=True)
     phone = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -13,6 +14,9 @@ class User(models.Model):
 
     class Meta:
         db_table = "users"
+        constraints = [
+            models.UniqueConstraint(Lower("email"), name="users_email_ci_unique"),
+        ]
 
     def __str__(self) -> str:
                     return self.full_name
